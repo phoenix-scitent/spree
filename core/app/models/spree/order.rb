74dd5889061f854f6befdb306a9e04ab46bdbd92
Jon Tiemann <jtiemann@digitalpersonae.com>
2012-11-10 22:48:46 -0500

    f-spree12: disappears delivery state, fixes translations, changes call to ransack helper sort_link -> jjj_sort_link due to metasearch overlap

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -14,7 +14,7 @@ module Spree
     include Spree::Order::Checkout
     checkout_flow do
       go_to_state :address
-      go_to_state :delivery
+      #go_to_state :delivery
       go_to_state :payment, :if => lambda { |order| order.payment_required? }
       go_to_state :confirm, :if => lambda { |order| order.confirmation_required? }
       go_to_state :complete
