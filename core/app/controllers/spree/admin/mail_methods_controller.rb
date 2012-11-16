8d12a97439caf048abc1ada9e95a8dc06e9a0200
Jon Tiemann <jtiemann@scitent.com>
2012-11-16 16:39:26 -0500

    fixes more translations (adds I18n prefix)

--- core/app/controllers/spree/admin/mail_methods_controller.rb
+++ core/app/controllers/spree/admin/mail_methods_controller.rb
@@ -12,13 +12,14 @@ module Spree
 
       def testmail
         @mail_method = Spree::MailMethod.find(params[:id])
+        binding.pry
         if TestMailer.test_email(@mail_method, try_spree_current_user).deliver
-          flash.notice = t('admin.mail_methods.testmail.delivery_success')
+          flash.notice = I18n.t('admin.mail_methods.testmail.delivery_success')
         else
-          flash[:error] = t('admin.mail_methods.testmail.delivery_error')
+          flash[:error] = I18n.t('admin.mail_methods.testmail.delivery_error')
         end
       rescue Exception => e
-        flash[:error] = t('admin.mail_methods.testmail.error') % {:e => e}
+        flash[:error] = I18n.t('admin.mail_methods.testmail.error') % {:e => e}
       ensure
         respond_with(@mail_method) { |format| format.html { redirect_to :back } }
       end
